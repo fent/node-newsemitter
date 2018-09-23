db4f4879208cb3eda34d3152a41f0b61548ec06e
fent <933490+fent@users.noreply.github.com>
2018-09-23 01:08:05 -0400

    test: test coverage

--- test/main-test.js
+++ test/main-test.js
@@ -3,19 +3,11 @@ const assert      = require('assert');
 
 
 describe('NEWS Emitter', () => {
-  const news = new NewsEmitter();
-  const results1 = [];
-  const results2 = [];
-
-  news.on('foo', (item) => {
-    results1.push(item);
-  });
-
-  news.on('bar', (item) => {
-    results2.push(item);
-  });
-
   it('Should not emit old items', () => {
+    const news = new NewsEmitter();
+    const results = [];
+    news.on('foo', (item) => results.push(item));
+
     assert.ok(news.emit('foo', { title: 'hello there' }));
     assert.ok(news.emit('foo', { title: 'hello world' }));
     assert.ok(!news.emit('foo', { title: 'hello there' }));
@@ -23,7 +15,7 @@ describe('NEWS Emitter', () => {
     assert.ok(!news.emit('foo', { title: 'hey' }));
     assert.ok(!news.emit('foo', { title: 'hey' }));
 
-    assert.deepEqual(results1, [
+    assert.deepEqual(results, [
       { title: 'hello there' },
       { title: 'hello world' },
       { title: 'hey' }
@@ -31,6 +23,13 @@ describe('NEWS Emitter', () => {
   });
 
   it('Keeps different history for each event', () => {
+    const news = new NewsEmitter();
+    const results1 = [];
+    const results2 = [];
+    news.on('foo', (item) => results1.push(item));
+    news.on('bar', (item) => results2.push(item));
+
+    assert.ok(news.emit('foo', { so: 'lucky' }));
     assert.ok(news.emit('bar', { so: 'lucky' }));
     assert.ok(news.emit('bar', { so: { so: 'lucky' } }));
     assert.ok(!news.emit('bar', { so: 'lucky' }));
@@ -44,28 +43,87 @@ describe('NEWS Emitter', () => {
       { title: 'hello world' }
     ]);
   });
+
+  it('Throws error with bad options', () => {
+    assert.throws(() => {
+      new NewsEmitter({ filter: { 'myevent': true } });
+    }, /must be an array/);
+    assert.throws(() => {
+      new NewsEmitter({ ignore: 'myotherevent' });
+    }, /must be an array/);
+    assert.throws(() => {
+      new NewsEmitter({ maxHistory: -10 });
+    }, /must be a positive integer/);
+    assert.throws(() => {
+      new NewsEmitter({ identifier: 'keyes' });
+    }, /must be a function/);
+  });
+
+  describe('`maxHistory` is reached', () => {
+    it('Older events are emitted again', () => {
+      const news = new NewsEmitter({ maxHistory: 1 });
+      const results = [];
+      news.on('aaa', (a) => results.push(a));
+
+      news.emit('aaa', 4);
+      news.emit('aaa', 5);
+      news.emit('aaa', 7);
+      news.emit('aaa', 4);
+      news.emit('aaa', 4);
+      news.emit('aaa', 4);
+      news.emit('aaa', 4);
+      news.emit('aaa', 4);
+      assert.deepEqual(results, [4, 5, 7, 4]);
+    });
+  });
+
+  describe('Use NewsEmitter#reset()', () => {
+    it('Emits events for resetted events', () => {
+      const news = new NewsEmitter();
+      const results1 = [];
+      const results2 = [];
+      news.on('foo', (item) => results1.push(item));
+      news.on('bar', (item) => results2.push(item));
+
+      assert.ok(news.emit('foo', { foo: 1, zee: 2 }));
+      assert.ok(news.emit('foo', 'movie'));
+
+      assert.ok(news.emit('bar', { so: 'lucky' }));
+      assert.ok(news.emit('bar', { so: { so: 'lucky' } }));
+      assert.ok(!news.emit('bar', { so: 'lucky' }));
+      assert.ok(!news.emit('bar', { so: { so: 'lucky' } }));
+
+      news.reset('bar');
+      assert.ok(news.emit('bar', { so: 'lucky' }));
+
+      news.reset();
+      assert.ok(news.emit('foo', { foo: 1, zee: 2 }));
+
+      assert.deepEqual(results1, [
+        { foo: 1, zee: 2 },
+        'movie',
+        { foo: 1, zee: 2 }
+      ]);
+      assert.deepEqual(results2, [
+        { so: 'lucky' },
+        { so: { so: 'lucky' } },
+        { so: 'lucky' },
+      ]);
+    });
+  });
 });
 
-
 describe('Filter events', () => {
-  const news = new NewsEmitter({ filter: ['live', 'forever'] });
-  const results1 = [];
-  const results2 = [];
-  const results3 = [];
-
-  news.on('live', (a, b, c) => {
-    results1.push([a, b, c]);
-  });
-
-  news.on('forever', (a, b, c) => {
-    results2.push([a, b, c]);
-  });
-
-  news.on('item', (item) => {
-    results3.push(item);
-  });
-
   it('Only filtered events are kept in history', () => {
+    const news = new NewsEmitter({ filter: ['live', 'forever'] });
+    const results1 = [];
+    const results2 = [];
+    const results3 = [];
+
+    news.on('live', (a, b, c) => results1.push([a, b, c]));
+    news.on('forever', (a, b, c) => results2.push([a, b, c]));
+    news.on('item', (item) => results3.push(item));
+
     news.emit('live', 'a', 'b');
     news.emit('forever', 'a', 'b');
     news.emit('live', 'a', 'b', 'c');
@@ -94,24 +152,18 @@ describe('Filter events', () => {
   });
 });
 
-
 describe('Custom identifier', () => {
-  const news = new NewsEmitter({
-    history: 50,
-    identifier: a => /^foo/.test(a[0]),
-  });
-  const results1 = [];
-  const results2 = [];
-
-  news.on('foo', (item) => {
-    results1.push(item);
-  });
-
-  news.on('bar', (item) => {
-    results2.push(item);
-  });
-
   it('Emits only items with different identifiers', () => {
+    const news = new NewsEmitter({
+      history: 50,
+      identifier: a => /^foo/.test(a[0]),
+    });
+    const results1 = [];
+    const results2 = [];
+
+    news.on('foo', (item) => results1.push(item));
+    news.on('bar', (item) => results2.push(item));
+
     news.emit('foo', 'what');
     news.emit('foo', 'what');
     news.emit('foo', 'butt');
@@ -129,34 +181,26 @@ describe('Custom identifier', () => {
   });
 });
 
-
 describe('Self manage history', () => {
   it('Is able to compare to history items we give it', () => {
-    const news = new NewsEmitter();
+    const news = new NewsEmitter({ manageHistory: true });
     const results1 = [];
-
-    news.on('foo', (a) => {
-      results1.push(a);
-    });
+    news.on('foo', (a) => results1.push(a));
 
     news.addHistory('foo', [['hello'], ['hello world']]);
-
     news.emit('foo', 'a');
     news.emit('foo', 'hello world');
     news.emit('foo', 'hello');
     news.emit('foo', 'a');
-
-    assert.deepEqual(results1, ['a']);
+    assert.deepEqual(results1, ['a', 'a']);
 
     news.addHistory('foo', [['b'], ['c']]);
-
     news.emit('foo', 'a');
     news.emit('foo', 'b');
     news.emit('foo', 'c');
     news.emit('foo', 'hello world');
     news.emit('foo', 'hello');
     news.emit('foo', 'indescrivable');
-
-    assert.deepEqual(results1, ['a', 'indescrivable']);
+    assert.deepEqual(results1, ['a', 'a', 'a', 'indescrivable']);
   });
 });
